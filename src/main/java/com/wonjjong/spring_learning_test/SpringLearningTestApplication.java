e8f39de689f45349c260b996808170a44b024535
원종민 <wjm358@kbrainc.com>
2024-11-29 16:35:33 +0900

    최초 커밋

---
+++ src/main/java/com/wonjjong/spring_learning_test/SpringLearningTestApplication.java
@@ -0,0 +1,13 @@
+package com.wonjjong.spring_learning_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonjjong/spring_learning_test/SpringLearningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonjjong.spring_learning_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
